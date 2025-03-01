2441e620f379957d6d66a1b2853f3a2e32c66263
Anirudh Aswal <152803490+Anirudh-ds@users.noreply.github.com>
2025-03-01 10:24:07 +0530

    Create Task01.py

---
+++ Assignment_01/Task01.py
@@ -0,0 +1,15 @@
+# Task 01 : Take 2 numbers and perform basic calculations on them
+print("Enter first number")
+number1 = int(input())
+print("Enter second number")
+number2 = int(input())
+
+add = number1 + number2
+sub = number1 - number2
+mul = number1 * number2
+div = number1 / number2
+
+print("Addition : ", add)
+print("Substraction : ", sub)
+print("Multiplication : ", mul)
+print("Division : ", div)
