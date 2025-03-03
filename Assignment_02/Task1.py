fa464f8b06a496f7de4b9ddbe5cd1b711e10ac15
Anirudh Aswal <152803490+Anirudh-ds@users.noreply.github.com>
2025-03-03 07:23:39 +0530

    Create Task1.py

---
+++ Assignment_02/Task1.py
@@ -0,0 +1,10 @@
+# Check if a number is even or odd
+
+print("Enter number : ")
+num = int(input())
+
+if(num % 2 == 0):
+    print(num, " is an even number")
+else:
+    print(num, " is an odd number")
+
