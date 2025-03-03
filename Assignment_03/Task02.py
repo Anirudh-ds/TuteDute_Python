61631b46c40a181eb82aef5de9b616b4483b0d09
Anirudh Aswal <152803490+Anirudh-ds@users.noreply.github.com>
2025-03-03 07:48:56 +0530

    Create Task02.py

---
+++ Assignment_03/Task02.py
@@ -0,0 +1,9 @@
+#Applying maths function
+import math
+
+print("Enter a number :")
+num = int(input())
+
+print("Square root : ", math.sqrt(num))
+print("Logarithm : ", math.log(num))
+print("Sine : ", math.sin(num))
